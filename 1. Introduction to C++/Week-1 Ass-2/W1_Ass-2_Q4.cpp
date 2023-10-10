3dab4610fcfdcf5e1ae8b0e8246238742cca41f6
Pratham Kocher <139618516+PrathamKocher@users.noreply.github.com>
2023-10-10 09:39:12 +0530

    Add files via upload

---
+++ 1. Introduction to C++/Week-1 Ass-2/W1_Ass-2_Q4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,h,vol,pi;
+    pi=3.14;
+    cout<<"Enter the radius of Cylinder: "<<endl;
+    cin>>r;
+    cout<<"Enter the height of the cylinder: "<<endl;
+    cin>>h;
+    vol=pi*r*r*h;
+    cout<<"The volume of the cylinder is: "<<vol;
+
+
+}   
